f9d8ab86b97f58cad6cffd2ef108baff6a31bb61
unknown <delirioushoran@gmail.com>
2021-07-09 18:04:53 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Computer Science Society</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="maincss.css">  
+
+        <!-- Script -->
+        <script type="text/javascript" src="univ.js"></script>
+
+    </head>
+
+    <body>
+    	
+    <div class="maincontainer">
+        <img src="/images/comsci.png" height="42" width="42">
+ 		<h3 style="margin-right: 30px;margin-left:10px;font-size: 30px;"> Computer Science Society </h3> 
+        <div class="vl"></div>
+		<ul>
+             <li><a href="home">HOME</a>          </li>
+			<li><a href="activities">ACTIVITIES</a>   </li>
+			<li> <a href="achievements">ACHIEVEMENTS</a>   </li>
+		</ul>
+
+	</div>
+    
+    <div class="scndcontainer">
+        <div class="rightcontainer">
+            <img src="/images/a1.jpg" alt="Avatar" class="image">
+            <div class="rightoverlay">
+            <div class="text">Welcome to</div>
+            </div>
+        </div>
+
+        <div class="leftcontainer">
+            <img src="/images/a2.jpg" alt="Avatar" class="image">
+            <div class="overlay">
+            <div class="text">Computer Science Society</div>
+            </div>
+        </div>
+    </div>
+    <div class="maincontainer">
+      <div class="contentcontainer">
+        <h1 style="margin-right:10px;margin-left:10px;"> OUR GOAL </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      </p>
+      </div>
+      <div class="contentcontainer">
+        <h1 style="margin-right:10px;margin-left:10px;"> OUR MISSION </h1>
+        <p>Mus mauris vitae ultricies leo integer malesuada. Faucibus 
+          et molestie ac feugiat sed lectus vestibulum. Amet mattis vulputate 
+          enim nulla aliquet porttitor lacus. Lorem donec massa sapien faucibus. 
+          Nullam non nisi est sit amet facilisis magna. 
+      </p>
+      </div>
+    </div>
+    </body>
+</html>
